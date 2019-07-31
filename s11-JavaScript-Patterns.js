c83342763cc10d205f50a4a575ae2c8405a85495
JCesana <joshuacesana@gmail.com>
2019-07-31 11:53:58 -0700

    Init

---
+++ s11-JavaScript-Patterns.js
@@ -0,0 +1,304 @@
+// Basic Structure
+
+// (function() {
+//     // Declare private vars and functions
+
+//     return {
+//         // Declare public var and functions
+//     }
+// })();
+
+// STANDARD MODULE PATTERN
+// const UICtrl = (function() {
+//     let text = 'Hello World';
+
+//     const changeText = function() {
+//         const element = document.querySelector('h1');
+//         element.textContent = text;
+//     };
+
+//     return {
+//         callChangeText: function() {
+//             changeText();
+//             console.log(text);
+//         }
+//     };
+// })();
+
+// UICtrl.callChangeText();
+
+// REVEALING MODULE PATTERN
+const ItemCtrl = (function() {
+    let data = [];
+
+    function add(item) {
+        data.push(item);
+        console.log('Item Added...');
+    }
+
+    function get(id) {
+        return data.find(item => {
+            return item.id === id;
+        });
+    }
+
+    return {
+        add: add,
+        get: get
+    };
+})();
+
+ItemCtrl.add({ id: 1, name: 'John' });
+console.log(ItemCtrl.get(1));
+
+// SINGLETON PATTERN
+const Singleton = (function() {
+    let instance;
+
+    function createInstance() {
+        const object = new Object({ name: 'Josh' });
+        return object;
+    }
+
+    return {
+        getInstance: function() {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
+
+console.log(instanceA);
+console.log(instanceB);
+console.log(instanceA === instanceB);
+
+// FACTORY PATTERN
+
+function MemberFactory() {
+    this.createMember = function(name, type) {
+        let member;
+
+        if (type === 'simple') {
+            member = new SimpleMembership(name);
+        } else if (type === 'standard') {
+            member = new StandardMembership(name);
+        } else if (type === 'super') {
+            member = new SuperMembership(name);
+        }
+
+        member.type = type;
+
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+        };
+
+        return member;
+    };
+}
+
+const SimpleMembership = function(name) {
+    this.name = name;
+    this.cost = '$5';
+};
+
+const StandardMembership = function(name) {
+    this.name = name;
+    this.cost = '$15';
+};
+
+const SuperMembership = function(name) {
+    this.name = name;
+    this.cost = '$25';
+};
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Doe', 'simple'));
+members.push(factory.createMember('Chris Jackson', 'super'));
+members.push(factory.createMember('Janice Williams', 'simple'));
+members.push(factory.createMember('Tom Smith', 'standard'));
+
+// console.log(members);
+
+members.forEach(function(member) {
+    member.define();
+});
+
+// OBSERVER PATTERN
+
+function EventObserver() {
+    this.observers = [];
+}
+
+EventObserver.prototype = {
+    subscribe: function(fn) {
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    },
+    unsubscribe: function(fn) {
+        this.observers = this.observers.filter(function(item) {
+            if (item !== fn) {
+                return item;
+            }
+        });
+        console.log(`You are now unsubscribed from ${fn.name}`);
+    },
+    fire: function() {
+        this.observers.forEach(function(item) {
+            item.call();
+        });
+    }
+};
+
+const click = new EventObserver();
+
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click', function() {
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+    click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function() {
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function() {
+    click.unsubscribe(getCurSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function() {
+    click.fire();
+});
+
+// Click Handler
+const getCurMilliseconds = function() {
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+};
+
+// Click Handler
+const getCurSeconds = function() {
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+};
+
+// OBSERVER PATTERN ES6
+
+class EventObserver {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(fn) {
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    }
+
+    unsubscribe(fn) {
+        this.observers = this.observers.filter(function(item) {
+            if (item !== fn) {
+                return item;
+            }
+        });
+        console.log(`You are now unsubscribed from ${fn.name}`);
+    }
+
+    fire() {
+        this.observers.forEach(function(item) {
+            item.call();
+        });
+    }
+}
+
+const click = new EventObserver();
+
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click', function() {
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+    click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function() {
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function() {
+    click.unsubscribe(getCurSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function() {
+    click.fire();
+});
+
+// Click Handler
+const getCurMilliseconds = function() {
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+};
+
+// Click Handler
+const getCurSeconds = function() {
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+};
+
+// MEDIATOR PATTERN
+const User = function(name) {
+    this.name = name;
+    this.chatroom = null;
+};
+
+User.prototype = {
+    send: function(message, to) {
+        this.chatroom.send(message, this, to);
+    },
+    receive: function(message, from) {
+        console.log(`${from.name} to ${this.name}: ${message}`);
+    }
+};
+
+const Chatroom = function() {
+    let users = {}; // list of users
+
+    return {
+        register: function(user) {
+            users[user.name] = user;
+            user.chatroom = this;
+        },
+        send: function(message, from, to) {
+            if (to) {
+                // Single user message
+                to.receive(message, from);
+            } else {
+                // Mass message
+                for (key in users) {
+                    if (users[key] !== from) {
+                        users[key].receive(message, from);
+                    }
+                }
+            }
+        }
+    };
+};
+
+const brad = new User('Brad');
+const jeff = new User('Jeff');
+const sarah = new User('Sarah');
+
+const chatroom = new Chatroom();
+
+chatroom.register(brad);
+chatroom.register(jeff);
+chatroom.register(sarah);
+
+brad.send('Hello Jeff', jeff);
+sarah.send('Hey Brad, you are the best dev ever!', brad);
+jeff.send('Hello Everyone!!');
